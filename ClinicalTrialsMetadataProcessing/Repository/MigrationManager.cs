fd94b648880907ebb8f5b883395027350a06a52e
ivan-djokic <73896183+ivan-djokic@users.noreply.github.com>
2024-12-12 02:31:27 +0100

    5
    5

---
+++ ClinicalTrialsMetadataProcessing/Repository/MigrationManager.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ClinicalTrialsMetadataProcessing.Repository;
+
+public static class MigrationManager
+{
+	public static void MigrateDatabase(this WebApplication app)
+	{
+		using var scope = app.Services.CreateScope();
+		using var context = scope.ServiceProvider.GetRequiredService<RepositoryContext>();
+		context.Database.Migrate();
+	}
+}
---
+++ ClinicalTrialsMetadataProcessing/Repository/RepositoryContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ClinicalTrialsMetadataProcessing.Models;
+
+namespace ClinicalTrialsMetadataProcessing.Repository;
+
+public class RepositoryContext(DbContextOptions<RepositoryContext> options) : DbContext(options)
+{
+	public DbSet<ClinicalTrialMetadata> ClinicalTrialsMetadata { get; set; }
+}
