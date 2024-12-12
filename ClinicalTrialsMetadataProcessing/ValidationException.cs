fd298d8545aa46cf54e73c80d009aee8accb4ad9
ivan-djokic <73896183+ivan-djokic@users.noreply.github.com>
2024-12-12 02:38:50 +0100

    Delete ClinicalTrialsMetadataProcessing/ValidationException.cs

--- ClinicalTrialsMetadataProcessing/ValidationException.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ClinicalTrialsMetadataProcessing.Utils;
-
-public class ValidationException(string format, params object?[] args) : Exception(string.Format(format, args))
-{
-	public ValidationException(string format, Exception ex)
-		: this(string.Format(format, ex.Message))
-	{
-	}
-}
