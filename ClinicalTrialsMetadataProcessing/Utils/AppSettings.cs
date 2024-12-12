e4f482b0fed2e4e96f3d47c200fc10f73a21eafb
ivan-djokic <73896183+ivan-djokic@users.noreply.github.com>
2024-12-12 02:39:10 +0100

    Add files via upload

---
+++ ClinicalTrialsMetadataProcessing/Utils/AppSettings.cs
@@ -0,0 +1,9 @@
+namespace ClinicalTrialsMetadataProcessing.Utils
+{
+    public static class AppSettings
+    {
+        public const string CONNECTION_STRING = "AppDbContext";
+
+        public const string OPTIONS = "AppOptions";
+	}
+}
---
+++ ClinicalTrialsMetadataProcessing/Utils/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using NJsonSchema.Validation;
+using System.Text;
+
+namespace ClinicalTrialsMetadataProcessing.Utils;
+
+public static class Extensions
+{
+	public static string GetString(this byte[] items)
+	{
+		return Encoding.Default.GetString(items);
+	}
+
+	public static bool IsEmpty<T>(this ICollection<T> items)
+	{
+		// Do not use items.Any() because of performance issues
+		return items.Count == 0;
+	}
+}
