5391763e400cf0a70927ceeebe2eea4159954c2b
ivan-djokic <73896183+ivan-djokic@users.noreply.github.com>
2024-12-12 02:27:54 +0100

    2
    2

---
+++ ClinicalTrialsMetadataProcessing/Models/ClinicalTrialsMetadata.cs
@@ -0,0 +1,12 @@
+﻿namespace ClinicalTrialsMetadataProcessing.Models;
+
+public class ClinicalTrialsMetadata
+{
+	public required ICollection<ClinicalTrialMetadata> Items { get; set; }
+
+	public required int Page { get; set; }
+
+	public required int PageSize { get; set; }
+
+	public required int TotalCount { get; set; }
+}
---
+++ ClinicalTrialsMetadataProcessing/Models/ClinicalTrialsMetadataStatus.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+using ClinicalTrialsMetadataProcessing.Utils;
+
+namespace ClinicalTrialsMetadataProcessing.Models;
+
+[JsonConverter(typeof(IgnoreWhiteSpacesEnumConverter<ClinicalTrialsMetadataStatus>))]
+public enum ClinicalTrialsMetadataStatus
+{
+	Completed = 0,
+	NotStarted = 1,
+	Ongoing = 2
+}
